3be6aa0ca3022ee49bbcd2b9b129664855c9ef40
tailuge <tailuge@gmail.com>
2026-02-16 16:49:44 +0000

    test(ui): Add basic ActivityPanel test

---
+++ src/components/ActivityPanel.test.tsx
@@ -0,0 +1,8 @@
+import { describe, test, expect } from "bun:test";
+import { ActivityPanel } from "./ActivityPanel";
+
+describe("ActivityPanel", () => {
+  test("should be defined", () => {
+    expect(ActivityPanel).toBeDefined();
+  });
+});
